6d9f0ed8d046bd6062f762f0de88da0b9f27762d
aarenstade <aarenstade@gmail.com>
2021-04-23 12:11:34 -0700

    0.0.7 (custom upload screens)

--- v2.py
+++ v2.py
@@ -1,6 +1,6 @@
 #### v2 FIRMWARE ####
 #### By Aaren Stade ####
-#### 0.0.5 ####
+#### 0.0.7 ####
 #### March 2021 ####
 
 import os
@@ -8,7 +8,7 @@ import pyrebase
 import requests
 import httplib2
 import tkinter as tk
-from PIL import ImageTk, Image
+from PIL import ImageTk, Image, ImageDraw, ImageFont
 
 #####  q#### CONSTANTS #########
 FIREBASE_CONFIG = {
@@ -25,7 +25,7 @@ WORKING_DIR = "/home/v2-firmware/"
 IMAGES_PATH = os.path.join(WORKING_DIR, "images/")
 SCREENS_PATH = os.path.join(WORKING_DIR, "screens/")
 FIRMWARE_FILE = os.path.join(WORKING_DIR, "firmware.txt")
-DISPLAY_TIME = 7000
+DISPLAY_TIME = 6500
 MAX_IMAGES = 5
 
 ######### VARIABLES #########
@@ -53,6 +53,41 @@ class Root_Window(object):
         self.tk.attributes('-fullscreen', False)
         self.tk.quit()
 
+###############################################
+# PROCESS UPLOAD SCREEN FUNCTIONS
+###############################################
+
+def get_old_price():
+    path = os.path.join(WORKING_DIR, 'price.txt')
+    if (os.path.exists(path)):
+        with open(path) as pricef:
+            price = pricef.read()
+        return str(price)
+
+def write_new_price(price):
+    path = os.path.join(WORKING_DIR, 'price.txt')
+    if (os.path.exists(path)):
+        os.remove(path)
+    with open(path, 'w') as pricef:
+        pricef.write(str(price))
+
+def generate_upload_screen(price):
+    file_path = os.path.join(SCREENS_PATH, 'upload.jpg')
+    if (os.path.exists(file_path)):
+        os.remove(file_path)
+    text = "${:,.2f}".format(price)
+    img = Image.open(os.path.join(WORKING_DIR, 'base-upload-screen.jpg'))
+    draw = ImageDraw.Draw(img)
+    fnt = ImageFont.truetype(os.path.join(WORKING_DIR, 'AlteHaasGroteskBold.ttf'), 300)
+    w, h = draw.textsize(text, font=fnt)
+    draw.text(((WINDOW_WIDTH-w)/2,((WINDOW_HEIGHT-h)/2) + 27), text, font=fnt,fill="black")
+    img.save(file_path)
+    write_new_price(price)
+
+###############################################
+# QUERY AND DISPLAY FUNCTIONS
+###############################################
+
 def create_tk_image(path):
     global current_image
     try:
@@ -63,7 +98,7 @@ def create_tk_image(path):
         img = ImageTk.PhotoImage(image=current_image)
         return img
     except:
-        print('image {} failed to open'.format(path), flush=True)
+        print('image {} failed to open'.format(path))
         return None
 
 def display_image(path):
@@ -76,33 +111,6 @@ def display_image(path):
         else:
             label = tk.Label(root, image=new_image, highlightthickness=0)
         label.pack(expand=True)  
-
-def init_images():
-    if not (os.path.exists(IMAGES_PATH)):
-        os.mkdir(IMAGES_PATH)
-    else:
-        global image_list
-        image_list = [f for f in os.listdir(IMAGES_PATH) if os.path.isfile(os.path.join(IMAGES_PATH, f))]
-
-def rotate_index():
-    global rot_index
-    if (rot_index >= len(image_list)):
-        rot_index = 0
-    else:
-        rot_index = rot_index + 1
-    print('rotated index to {}'.format(rot_index), flush=True)
-    return rot_index
-
-def check_connection(url='www.google.com', timeout=15):
-    req = httplib2.HTTPConnectionWithTimeout(url, timeout=timeout);
-    try:
-        req.request('HEAD', "/")
-        req.close()
-        print('connected')
-        return True
-    except Exception as e:
-        print('no connection: {}'.format(e), flush=True)
-        return False;
 
 def append_image_list(filename):
     global image_list
@@ -117,9 +125,9 @@ def download_image(filename, url):
         global image_list
         file_path = os.path.join(IMAGES_PATH, filename)
         request = requests.get(url, stream=True)
-        print('downloading image...', flush=True)
+        print('downloading image...')
         if not request.ok:
-            print(request.text, flush=True)
+            print(request.text)
         with open(file_path, 'wb') as img_file:
             for block in request.iter_content(1024):
                 if not block:
@@ -141,21 +149,68 @@ def delete_image(delete_filename):
         image_list.remove(delete_filename)
 
 def handle_query(data):
-    upload_id = data['image']['uuid']
-    upload_url = data['image']['url']
-    upload_filename = upload_id + '.jpg'
-    delete_id = data['delete']['uuid']
-    delete_filename = delete_id + '.jpg'
-    if not (upload_filename in image_list):
-        download_image(upload_filename, upload_url)
-    if (delete_filename in image_list):
-        delete_image(delete_filename)
+    try:
+        upload_id = data['image']['uuid']
+        upload_url = data['image']['url']
+        upload_filename = upload_id + '.jpg'
+        if not (upload_filename in image_list):
+            download_image(upload_filename, upload_url)
+    except:
+        pass
+
+def handle_query(data):
+    try:
+        upload_id = data['image']['uuid']
+        upload_url = data['image']['image']
+        upload_filename = upload_id + '.jpg'
+        if not (upload_filename in image_list):
+            download_image(upload_filename, upload_url)
+    except:
+        pass
+    try:
+        price = data['price']
+        old_price = get_old_price()
+        if (str(price) != old_price):
+            generate_upload_screen(price)
+    except:
+        pass
 
 def query_for_new_ads():
     query = db.child(BILLBOARD_ID).get()
     data = query.val()
     if (data != None):
         handle_query(data)
+
+###############################################
+# MAIN CONTROL FUNCTIONS
+###############################################
+
+def check_connection(url='www.google.com', timeout=15):
+    req = httplib2.HTTPConnectionWithTimeout(url, timeout=timeout);
+    try:
+        req.request('HEAD', "/")
+        req.close()
+        print('connected')
+        return True
+    except Exception as e:
+        print('no connection: {}'.format(e))
+        return False;
+
+def rotate_index():
+    global rot_index
+    if (rot_index >= len(image_list)):
+        rot_index = 0
+    else:
+        rot_index = rot_index + 1
+    print('rotated index to {}'.format(rot_index))
+    return rot_index
+
+def init_images():
+    if not (os.path.exists(IMAGES_PATH)):
+        os.mkdir(IMAGES_PATH)
+    else:
+        global image_list
+        image_list = [f for f in os.listdir(IMAGES_PATH) if os.path.isfile(os.path.join(IMAGES_PATH, f))]
 
 def loop():
     if (rot_index != len(image_list)):
@@ -172,7 +227,6 @@ def loop():
 def setup():
     Root_Window(root)
     init_images()
-    display_image(os.path.join(SCREENS_PATH, "launch.jpg"))
 
 if __name__ == "__main__":
     setup()
